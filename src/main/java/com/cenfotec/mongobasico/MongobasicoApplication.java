8c65f834538fc617765f99e9fe791828608d2abf
Moralitos28 <carlos-morales28@hotmail.es>
2021-07-30 21:08:22 -0600

    Initial commit

---
+++ src/main/java/com/cenfotec/mongobasico/MongobasicoApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.mongobasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobasicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongobasicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cenfotec/mongobasico/MongobasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.mongobasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongobasicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
