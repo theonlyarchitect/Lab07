6d18c4e5913646fa5a394015ac236d416fa04b1f
Moralitos28 <carlos-morales28@hotmail.es>
2021-07-31 02:10:25 -0600

    Adedd rq files

---
+++ src/main/java/com/cenfotec/mongobasico/service/JournalServiceImpl.java
@@ -0,0 +1,28 @@
+package com.cenfotec.mongobasico.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cenfotec.mongobasico.domain.Journal;
+import com.cenfotec.mongobasico.repository.JournalRepository;
+@Service
+public class JournalServiceImpl implements JournalService {
+    @Autowired
+    JournalRepository journalRepo;
+    @Override
+    public List<Journal> getAllJournals() {
+        return journalRepo.findAll();
+    }
+    @Override
+    public void saveJournal(Journal newJournal) {
+        journalRepo.save(newJournal);
+    }
+    @Override
+    public List<Journal> findJournalsByTitle(String title) {
+        return journalRepo.findByTitleLike(title);
+    }
+    @Override
+    public void deleteAllJournals() {
+        journalRepo.deleteAll();
+    }
+}
